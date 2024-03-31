26cab498d3e0a5d3660ee6a641b3170309fb1dd1
mahishamudaliar26 <mahishamudaliar2603@gmail.com>
2024-03-31 18:45:57 +0530

    headings and button

--- educity/src/App.jsx
+++ educity/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
+import Hero from "./components/Hero/Hero";
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <Hero />
     </div>
   );
 };
