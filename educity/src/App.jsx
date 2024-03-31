227d1e5b6d689025edab768f6b67f1544639fb54
mahishamudaliar26 <mahishamudaliar2603@gmail.com>
2024-03-31 23:37:46 +0530

    media query

--- educity/src/App.jsx
+++ educity/src/App.jsx
@@ -7,6 +7,7 @@ import About from "./components/About/About";
 import Campus from "./components/Campus/Campus";
 import Testimonials from "./components/Testimonials/Testimonials";
 import Contact from "./components/Contacts/Contact";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
@@ -26,6 +27,7 @@ const App = () => {
         <Testimonials />
         <Title subTitle="Contact Us" title="Get in Touch" />
         <Contact />
+        <Footer />
       </div>
     </div>
   );
--- educity/src/components/NavBar/NavBar.jsx
+++ educity/src/components/NavBar/NavBar.jsx
@@ -1,26 +1,71 @@
 import React, { useEffect, useState } from "react";
 import logo from "../../assets/logo.png";
+import { Link } from "react-scroll"; // Correct import
 import "../NavBar/NavBar.css";
+import menu_icon from "../../assets/menu-icon.png";
+
 const NavBar = () => {
   const [sticky, setSticky] = useState(false);
+
   useEffect(() => {
-    window.addEventListener("scroll", () => {
+    const handleScroll = () => {
       window.scrollY > 700 ? setSticky(true) : setSticky(false);
-    });
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+  };
+
   return (
     <nav className={`container ${sticky ? "dark-nav" : ""}`}>
       <img src={logo} alt="" className="logo" />
-      <ul>
-        <li>Home</li>
-        <li>Program</li>
-        <li>About Us</li>
-        <li>Campus</li>
-        <li>Testimonals</li>
+      <ul className={mobileMenu ? "" : "hide-mobile-menu"}>
         <li>
-          <button className="btn">Contact Us</button>
+          {" "}
+          <Link to="hero" smooth={true} offset={0} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="program" smooth={true} offset={-260} duration={500}>
+            Program
+          </Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} offset={-150} duration={500}>
+            About Us
+          </Link>
+        </li>
+        <li>
+          <Link to="campus" smooth={true} offset={-260} duration={500}>
+            Campus
+          </Link>
+        </li>
+        <li>
+          <Link to="testimonial" smooth={true} offset={-260} duration={500}>
+            Testimonials
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="contact"
+            smooth={true}
+            offset={-260}
+            duration={500}
+            className="btn"
+          >
+            Contact Us
+          </Link>
         </li>
       </ul>
+      <img src={menu_icon} className="menu-icon" onClick={toggleMenu} />
     </nav>
   );
 };
