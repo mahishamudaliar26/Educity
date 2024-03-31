499afdca162a0eacc8b0065fa5e77c35b41eb79a
mahishamudaliar26 <mahishamudaliar2603@gmail.com>
2024-03-31 19:45:03 +0530

    Program files

--- educity/src/App.jsx
+++ educity/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
 import Hero from "./components/Hero/Hero";
+import Programs from "./components/Program/Programs";
 
 const App = () => {
   return (
     <div>
       <NavBar />
       <Hero />
+      <div className="container">
+        <Programs />
+      </div>
     </div>
   );
 };
