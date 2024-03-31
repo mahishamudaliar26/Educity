dfd9401f281cdf9bf3e3d1fbd9779797e5bc9657
mahishamudaliar26 <mahishamudaliar2603@gmail.com>
2024-03-31 20:10:50 +0530

    Title

--- educity/src/App.jsx
+++ educity/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "./components/NavBar/NavBar";
 import Hero from "./components/Hero/Hero";
 import Programs from "./components/Program/Programs";
+import Title from "./components/Title/Title";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <NavBar />
       <Hero />
       <div className="container">
+        <Title subTitle="OUR PROGRAM" title="What we offer" />
         <Programs />
       </div>
     </div>
